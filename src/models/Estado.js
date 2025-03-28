67d2feadc2d50bfe9a959e8a54279222d51bdafa
Matheus C <math.pi.cardoso@gmail.com>
2025-03-28 11:02:26 -0300

    feat: Validações de Estado acrescentadas

--- src/models/Estado.js
+++ src/models/Estado.js
@@ -3,12 +3,42 @@ import { Model, DataTypes } from 'sequelize';
 class Estado extends Model {
   static init(sequelize) {
     super.init({
-      data: DataTypes.DATE,
-      peso: DataTypes.FLOAT,
-      altura: DataTypes.FLOAT,
-      taxaGordura: DataTypes.FLOAT,
-      circunferenciaCintura: DataTypes.FLOAT,
-      circunferenciaBraco: DataTypes.FLOAT,
+      data: { 
+        type: DataTypes.DATEONLY, 
+        validate: {
+          isDate: { msg: "Data do Estado deve seguir o formato YYYY-MM-DD." }
+        }
+      },
+      peso: { 
+        type: DataTypes.FLOAT, 
+        validate: {
+          isFloat: { msg: "Peso deve estar em kilos, em um valor numérico decimal." }
+        }
+      },
+      altura: { 
+        type: DataTypes.FLOAT, 
+        validate: {
+          isFloat: { msg: "Altura deve estar em metros, em um valor numérico decimal." }
+        }
+      },
+      taxaGordura: { 
+        type: DataTypes.FLOAT, 
+        validate: {
+          isFloat: { msg: "Taxa de gordura deve ser um valor numérico decimal." }
+        }
+      },
+      circunferenciaCintura: { 
+        type: DataTypes.FLOAT, 
+        validate: {
+          isFloat: { msg: "A circunferência da cintura deve estar em centímetros, em um valor numérico decimal." }
+        }
+      },
+      circunferenciaBraco: { 
+        type: DataTypes.FLOAT, 
+        validate: {
+          isFloat: { msg: "A circunferência do braço deve estar em centímetros, em um valor numérico decimal." }
+        }
+      },
       comentarios: DataTypes.STRING
     }, { sequelize, modelName: 'estado', tableName: 'estados' });
   }
--- src/models/Nutricionista.js
+++ src/models/Nutricionista.js
@@ -3,10 +3,33 @@ import { Model, DataTypes } from 'sequelize';
 class Nutricionista extends Model {
   static init(sequelize) {
     super.init({
-      nome: DataTypes.STRING,
-      email: DataTypes.STRING,
-      telefone: DataTypes.STRING,
-      horarioAtendimento: DataTypes.INTEGER
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Nome não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Nome pode ter no máximo 50 caracteres." },
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Email não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Email pode ter no máximo 50 caracteres." },
+        }
+      },
+      telefone: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Telefone não pode ficar vazio." },
+          is: {args: /^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}/, msg: "Telefone deve seguir o padrão (NN) NNNNN-NNNN" },
+        }
+      },
+      horarioAtendimento: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Horário de atendimento não pode ficar vazio." },
+        }
+      },
     }, { sequelize, modelName: 'nutricionista', tableName: 'nutricionistas' });
   }
 }
