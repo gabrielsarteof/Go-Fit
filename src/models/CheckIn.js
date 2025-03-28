aa8a45dd9e668aa06cb753df85dba93f3280c2d2
arthurSinja <sinjaarthur@gmail.com>
2025-03-28 11:05:24 -0300

    atualizacoes

--- src/models/CheckIn.js
+++ src/models/CheckIn.js
@@ -3,12 +3,21 @@ import { Model, DataTypes } from 'sequelize';
 class CheckIn extends Model {
   static init(sequelize) {
     super.init({
-      entrada: DataTypes.TIME,
-      saida: DataTypes.TIME,
-      acessoAutorizado: DataTypes.BOOLEAN,
-      razaoBloqueio: DataTypes.STRING
-    }, { sequelize, modelName: 'checkIn', tableName: 'checkins' });
+      entrada: {
+        type: DataTypes.DATE,
+      },
+      saida: {
+        type: DataTypes.DATE,
+      },
+      acessoAutorizado: {
+        type: DataTypes.BOOLEAN,
+      },
+      razaoBloqueio: {
+        type: DataTypes.STRING,
+      }
+    }, { 
+      sequelize, modelName: 'checkIn', tableName: 'checkins' });
   }
 }
 
-export { CheckIn};
+export { CheckIn };
--- src/models/Dieta.js
+++ src/models/Dieta.js
@@ -3,11 +3,33 @@ import { Model, DataTypes } from 'sequelize';
 class Dieta extends Model {
   static init(sequelize) {
     super.init({
-      descricao: DataTypes.STRING,
-      dataCriacao: DataTypes.DATE,
-      dataExpiracao: DataTypes.DATE,
-      instrucoes: DataTypes.STRING
-    }, { sequelize, modelName: 'dieta', tableName: 'dietas' });
+      descricao: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'A descrição é obrigatória' },
+          notEmpty: { msg: 'A descrição não pode ser vazia' },
+          len: {
+            args: [3, 255],
+            msg: 'A descrição deve ter entre 3 e 255 caracteres'
+          }
+        }  
+      },  
+      dataCriacao: {
+        type: DataTypes.DATE,
+      },
+      dataExpiracao: {
+        type: DataTypes.DATE,
+      },
+      instrucoes: {
+        type: DataTypes.STRING,
+        allowNull: false,  
+        validate: {
+          notEmpty: { msg: "Instruções não podem ser vazias" }
+        }
+      }
+    }, { 
+      sequelize, modelName: 'dieta', tableName: 'dietas' });
   }
 }
 
