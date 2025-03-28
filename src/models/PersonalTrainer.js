f2f201477bf653a8f5ff6a8a222e3f5126253315
Matheus C <math.pi.cardoso@gmail.com>
2025-03-28 10:00:01 -0300

    feat: Validações do Nutricionista acrescentadas

--- src/models/PersonalTrainer.js
+++ src/models/PersonalTrainer.js
@@ -3,12 +3,42 @@ import { Model, DataTypes } from 'sequelize';
 class PersonalTrainer extends Model {
   static init(sequelize) {
     super.init({
-      nome: DataTypes.STRING,
-      certificacao: DataTypes.STRING,
-      email: DataTypes.STRING,
-      telefone: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Nome não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Nome pode ter no máximo 50 caracteres." },
+        }
+      },
+      certificacao: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Certificação não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Certificação pode ter no máximo 50 caracteres." },
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Email não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Email pode ter no máximo 50 caracteres." },
+        }
+      },
+      telefone: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Telefone não pode ficar vazio." },
+          is: {args: /^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}/, msg: "Telefone deve seguir o padrão (NN) NNNNN-NNNN" },
+        }
+      },
       horarioAtendimento: DataTypes.STRING,
-      especialidade: DataTypes.STRING
+      especialidade: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "Especialidade não pode ficar vazio." },
+          len: {args: [1, 50],  msg: "Especialidade pode ter no máximo 50 caracteres." },
+        }
+      },
     }, { sequelize, modelName: 'personalTrainer', tableName: 'personais' });
   }
 }
