a34d8ba28048eb6bd3626dcbae1b380aecf920c9
gabrielsarteof <gabrielsarteof@gmail.com>
2025-04-04 10:53:52 -0300

    feat: importa routes ao server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import routes from './routes.js';
 
 // Importando configuração e estabelecimento da conexão com o banco de dados
 import sequelize from './config/database-connection.js';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
