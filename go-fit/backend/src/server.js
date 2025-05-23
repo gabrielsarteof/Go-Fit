cd8166fbf3e5c933c3899bc43d82c4c5b46d18f4
Matheus C <math.pi.cardoso@gmail.com>
2025-05-23 10:22:14 -0300

    feat: inserido o error handler, criada a pasta _middleware, app agora usando o error-handler

--- go-fit/backend/src/server.js
+++ go-fit/backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from './routes.js';
+import errorHandler from '../src/_middleware/error-handler.js';
 
 // Importando configuração e estabelecimento da conexão com o banco de dados
 import sequelize from './config/database-connection.js';
@@ -7,5 +8,6 @@ import sequelize from './config/database-connection.js';
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333);
